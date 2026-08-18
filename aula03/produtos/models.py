ad3d575795b764ad869b8ee6a13003e03c32ee5f
Gui-Rocha06 <guioliveirarocha06@gmail.com>
2026-08-18 19:58:04 -0300

    Aula 3

---
+++ aula03/produtos/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Produto (models.Model):
+    nome = models.ChartField(max_length=120) # definindo o tamanho do nome do produto com tamanho máximo de 120 caracteres
+    quantidade = models.PositiveIntegerField(default=0) # quantidade do produto
+    preco = models.DecimalField(max_digits=10,decimal_places=2) # definindo a qtde de digitos e casas decimais
+    created_at = models.DateTimeField(auto_now_add = true) # registro de tempo automático quando o produto é carregado
+
+
+    # cria a função
+    def __str__(self):
+        return f"{self.nome} (qtde={self.quantidade})"
