97320320214fd2561bd6ac8a850ad48577c6aa99
Gui-Rocha06 <guioliveirarocha06@gmail.com>
2026-08-18 20:44:52 -0300

    Migrações funcionando

--- aula03/loja/settings.py
+++ aula03/loja/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-65u=!^n$kagnoa=#v$9&uqkzo3v30hk^z(fsn!q&9o_s1n4z+(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','127.0.0.1']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'produtos'
 ]
 
 MIDDLEWARE = [
--- aula03/produtos/models.py
+++ aula03/produtos/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Produto (models.Model):
-    nome = models.ChartField(max_length=120) # definindo o tamanho do nome do produto com tamanho máximo de 120 caracteres
+    nome = models.CharField(max_length=120) # definindo o tamanho do nome do produto com tamanho máximo de 120 caracteres
     quantidade = models.PositiveIntegerField(default=0) # quantidade do produto
     preco = models.DecimalField(max_digits=10,decimal_places=2) # definindo a qtde de digitos e casas decimais
-    created_at = models.DateTimeField(auto_now_add = true) # registro de tempo automático quando o produto é carregado
+    created_at = models.DateTimeField(auto_now_add = True) # registro de tempo automático quando o produto é carregado
 
 
     # cria a função
