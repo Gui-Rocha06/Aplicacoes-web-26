8bc82e0e0f79e1d0fa14fbde00ce105222621839
Gui-Rocha06 <guioliveirarocha06@gmail.com>
2026-09-08 20:56:38 -0300

    Aula 07 - Criando a proteção das rotas para fazer pedido

--- aula03/produtos/views.py
+++ aula03/produtos/views.py
@@ -4,12 +4,18 @@ from .models import Pedido
 from .serializers import StatusPedidoSerializer
 from rest_framework import mixins
 
-from .serializers import (ProdutoSerializer, CategoriaSerializer, ClienteSerializer, PedidoSerializer, ItemPedidoSerializer) 
+from .serializers import (ProdutoSerializer, CategoriaSerializer, ClienteSerializer, PedidoSerializer, ItemPedidoSerializer,StatusPedidoSerializer) 
 from .models import (Produto, Categoria, Cliente, Pedido, ItemPedido)  
-
+from rest_framework.exceptions import ValidationError
 from rest_framework.permissions import(
     AllowAny, IsAuthenticated
 )
+
+from django.contrib.auth.models import User
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+from .serializers import CadastroUsuarioSerializers
+
 # importando metodo para exibir uma pagina home
 
 from django.http import HttpResponse
@@ -17,24 +23,35 @@ from django.http import HttpResponse
 def home(request):
     return HttpResponse("Olá Django ! Aplicações Web 2026 -2 - Aula 05 Loja de Produtos")
 
+
+
+
+class CadastroUsuarioView(generics.CreateAPIView):
+    
+    queryset = User.objects.all()
+    serializer_class = CadastroUsuarioSerializers
+    permission_classes = [AllowAny]
+
+
     # Cria a classe Produtoviewset responsável por permitir fazer o crude
 
 class ProdutoViewSet(viewsets.ModelViewSet):
     queryset = Produto.objects.all().order_by("-id")
     serializer_class = ProdutoSerializer
-
-    # Função de permissão
-
-    def get_permissions(self):
-
-        # Permite que qualquer pessoa consulte os produtos
-        if self.action in ['list', 'retriever']:
-
-            return [AllowAny()]
-
-        # Para poder cadastrar, editar ou excluir o usuário precisa estar autenticado
     
+    # função de permissao
+    
+    def get_permissions(self):
+        
+        # Permite que qualquer pessoa consulte produtos
+        if self.action in ['list', 'retrieve']:
+            
+            return [AllowAny()]
+        
+        # Para poder cadastrar, editar ou excluir o usuario precisa estar autenticado
+        
         return [IsAuthenticated()]
+
 
 # Categoria
 
@@ -51,12 +68,7 @@ class ClienteViewSet(viewsets.ModelViewSet):
     serializer_class = ClienteSerializer
     
     
-# Pedido
-
-class PedidoViewSet(viewsets.ModelViewSet):
-    queryset = Pedido.objects.all().order_by("-id")
-    serializer_class = PedidoSerializer
-    
+   
     
 # ItemPedido
 
@@ -65,13 +77,50 @@ class ItemPedidoViewSet(viewsets.ModelViewSet):
     
     queryset = ItemPedido.objects.all().order_by("-id")
     serializer_class = ItemPedidoSerializer
+# Create your views here.
 
-class StatusPedidoViewSet (
+
+class PedidoViewSet(viewsets.ModelViewSet):
+    queryset = Pedido.objects.all()
+    serializer_class = PedidoSerializer
+    permission_classes = [IsAuthenticated]
+    
+    # Mostra somente os pedidos do usuario autenticado
+    
+    def get_queryset(self):
+        
+        return Pedido.objects.filter(
+            cliente_usuario = self.request.user).order_by("-id")
+        
+        
+    # Ao criar um pedido, associa automaticamente ao cliente autenticado
+    
+    def perform_create(self, serializer):
+        
+        try: 
+        
+             cliente = Cliente.objects.get(
+             usuario = self.request.user
+        )
+             
+        except Cliente.DoesNotExist:
+            raise ValidationError ("O usuario autenticado não possui um cliente associado")
+        
+        serializer.save(cliente =cliente)
+        
+
+
+
+
+
+
+
+class StatusPedidoViewSet(
     mixins.ListModelMixin,
     mixins.RetrieveModelMixin,
     mixins.UpdateModelMixin,
     viewsets.GenericViewSet
 ):
+
     queryset = Pedido.objects.all()
     serializer_class = StatusPedidoSerializer
-    
